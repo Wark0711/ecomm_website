324612179bd63d147f0f2c7be761fe939dfb44a2
Wark0711 <mickey.rahul98@gmail.com>
2024-02-19 23:42:11 +0530

    Adjusted homepage for mobile devices.

--- src/app/footer.jsx
+++ src/app/footer.jsx
@@ -8,9 +8,9 @@ import Link from 'next/link';
 export default function Footer() {
     return (
         <Box>
-            <Box sx={{ backgroundImage: `url(${logo.src})`, backgroundPosition: 'center center', borderRadius: 1, minHeight: 300, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <Box sx={{ backgroundImage: `url(${logo.src})`, backgroundPosition: 'center center', borderRadius: 1, minHeight: { xs: 200, sm: 300 }, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                 <Box className={styles['ft-box']}>
-                    <Box sx={{ fontSize: 24, fontWeight: 500 }}>Connect with Us</Box>
+                    <Box sx={{ fontSize: { xs: 21, sm: 24 }, fontWeight: 500 }}>Connect with Us</Box>
                     <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: 2, flexWrap: 'wrap' }}>
                         <Link className='nav-link' href={'#'}>
                             <Box className={styles.ft}>
@@ -41,7 +41,7 @@ export default function Footer() {
                 </Box>
             </Box>
             <Box>
-                <Box sx={{ display: 'flex', alignItems: 'top', justifyContent: 'space-around', gap: 2, flexWrap: 'wrap', my: 8 }}>
+                <Box sx={{ display: 'flex', alignItems: 'top', justifyContent: 'space-around', gap: 4, flexWrap: 'wrap', my: 8 }}>
                     <Box>
                         <Box sx={{ color: '#3a3a3a', fontWeight: 500, fontSize: 19 }}>Shop</Box>
                         <Box mt={2}>
@@ -97,9 +97,9 @@ export default function Footer() {
                 </Box>
             </Box>
             <Box>
-                <Box sx={{ backgroundColor: '#3a3a3a', color: '#ffffff', fontSize: '14px', borderRadius: 1, display: 'flex', alignItems: 'top', justifyContent: 'space-between', }}>
-                    <Box fontFamily={'Poppins, sans-serif'} p={4}>Copyright &copy; {`${new Date().getFullYear()}`} WDT Express</Box>
-                    <Box fontFamily={'Poppins, sans-serif'} p={4}>Powered by WDT Express</Box>
+                <Box sx={{ backgroundColor: '#3a3a3a', color: '#ffffff', fontSize: '14px', borderRadius: 1, display: 'flex', alignItems: 'top', justifyContent: { xs: 'center', sm: 'space-between' }, flexWrap: 'wrap' }}>
+                    <Box fontFamily={'Poppins, sans-serif'} pt={{ xs: 2.5, sm: 3 }} px={2}>Copyright &copy; {`${new Date().getFullYear()}`} WDT Express</Box>
+                    <Box fontFamily={'Poppins, sans-serif'} p={{ xs: 2.5, sm: 3 }} px={2}>Powered by WDT Express</Box>
                 </Box>
             </Box>
         </Box>
